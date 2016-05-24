396b18dc08bc2d407eb7b55239181af73820d05a
Jack Hardy <jackhardy1@gmail.com>
2016-05-24 17:28:53 +0100

    can leave a review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,12 +3,12 @@ class ReviewsController < ApplicationController
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
-    redirect_to new_restaurant_review_path(restaurant)
   end
 
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @restaurant.reviews.create(review_params)
+    redirect_to '/restaurants'
   end
 
   def review_params
